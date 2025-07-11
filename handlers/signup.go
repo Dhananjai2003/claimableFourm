f26fcfd0ed63210380c74add77324611746f7355
Dhananjai2003 <dhananjaimurali30@gmail.com>
2025-07-11 14:15:46 +0530

    completed signup.go

---
+++ handlers/signup.go
@@ -0,0 +1,27 @@
+package handlers
+
+import (
+	"claimable-forum/db"
+	"claimable-forum/models"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+
+func SignUp(c *gin.Context) {
+
+	var input models.User
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input"})
+		return
+	}
+
+	_, err := db.DB.Exec(`INSERT INTO users (username, password_hash) VALUES ($1, $2)`, input.Username, input.Password_hash)
+	if err != nil {
+		c.JSON(http.StatusConflict, gin.H{"error": "Username already exists"})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"message": "User created"})
+
+}
