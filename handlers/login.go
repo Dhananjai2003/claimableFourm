52db8b24614ea8b5a7767fa3754782e7b0960693
Dhananjai2003 <dhananjaimurali30@gmail.com>
2025-07-11 14:18:31 +0530

    completed login handler

---
+++ handlers/login.go
@@ -0,0 +1,34 @@
+package handlers
+
+import (
+	"claimable-forum/db"
+	"claimable-forum/models"
+	"claimable-forum/utils"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+
+func Login(c *gin.Context) {
+	var input models.User
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input"})
+		return
+	}
+
+	var user models.User
+	err := db.DB.QueryRow(`SELECT id, password_hash FROM users WHERE username=$1`, input.Username).
+		Scan(&user.ID, &user.Password)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
+		return
+	}
+
+	if !utils.CheckPassword(input.Password, user.Password) {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
+		return
+	}
+
+	token, _ := utils.GenerateJWT(user.ID)
+	c.JSON(http.StatusOK, gin.H{"token": token})
+}
