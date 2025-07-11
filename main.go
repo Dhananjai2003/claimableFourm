ec21c57ae20331ee55a89d468b5e1028eae1ac62
Dhananjai2003 <dhananjaimurali30@gmail.com>
2025-07-11 13:11:23 +0530

    added routes

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main 
 
 import (
-	"fmt"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 
+	r := gin.Default()
+
+	r.Run(":8080")
 
 }
---
+++ routes/routes.go
@@ -0,0 +1,11 @@
+package routes
+
+import (
+	"claimable-forum/handlers"
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRoutes(r *gin.Engine) {
+	r.POST("/signup", handlers.SignUp)
+	r.POST("/login", handlers.Login)
+}
