0f10dfcbd605645feec8f8817e13c2afceb0afbd
Dhananjai2003 <dhananjaimurali30@gmail.com>
2025-07-16 14:08:16 +0530

    made jwt to jwtGen and jwtParse and made parser for jwt token

---
+++ utils/jwtParse.go
@@ -0,0 +1,25 @@
+package utils
+
+import (
+	"github.com/golang-jwt/jwt/v5"
+	"time"
+	"os"
+)
+
+func ParseJWT(tokenStr string) (jwt.MapClaims, error) {
+	token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, jwt.ErrInvalidKeyType
+		}
+		return secretKey, nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return claims, nil
+	}
+	return nil, jwt.ErrTokenInvalid
+}
